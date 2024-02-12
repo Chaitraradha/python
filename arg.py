a54b4e0dfae0e61bee059af116469e8ac4ef79bf
Ubuntu <ubuntu@ip-172-31-36-13.ap-southeast-2.compute.internal>
2024-02-12 08:40:11 +0000

    move

---
+++ arg.py
@@ -0,0 +1,13 @@
+import sys
+sys.argv
+print(sys.argv)
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.path)
+#sys.exit(f"the execution was intentionally terminated")
+print("hello")
+var="hello"
+if var == "hi":
+    print(sys.exit())
+       # else
+       #print(sys.exit(f"stop the exection")
